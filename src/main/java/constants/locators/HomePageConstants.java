42132816decb88262b6f5bdb8cffbbc42d23481d
MariamKhalatova <mar.khalatova@gmail.com>
2022-09-30 22:51:28 +0400

    Removed unused constnat

--- src/main/java/constants/locators/HomePageConstants.java
+++ src/main/java/constants/locators/HomePageConstants.java
@@ -4,7 +4,7 @@ public class HomePageConstants {
     public static final String citiesFilterID = "jobsfilter-job_city";
     public static final String searchButton = "//form[1]/div[1]/div[5]";
     public static final String JobsRadioButton = "jobsfilter-type";
-    public static final String testt = "//label[1]/div[1]/input[1]";
+    
     //Header elements
     public static final String forJobSeekersDrp = "//div[2]/div[1]/ul[1]/li[6]/a[1]";
     public static final String registerButton = "//div[2]/div[1]/ul[1]/li[6]/ul/li[2]";
